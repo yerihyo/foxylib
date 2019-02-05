523735537043dd6fdb3c88f872f5f3c550ca48ca
yerihyo <yerihyo@gmail.com>
2019-02-04 22:05:34 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+VERSION="0.2.0"
+
+setuptools.setup(
+    name="foxylib",
+    version=VERSION,
+    description="First package",
+    author="Moonyoung Kang",
+    author_email="yerihyo@gmail.com",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/foxytrixy-com/foxlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
