c760d5199b23b298acdce9746e836c5a96de70f6
yerihyo <yerihyo@gmail.com>
2019-02-21 22:22:38 -0800

    improve logger_tools

--- foxylib/tools/log/logger_tools.py
+++ foxylib/tools/log/logger_tools.py
@@ -1,11 +1,10 @@
-import inspect
 import logging
-import os
 from datetime import datetime
 from functools import wraps
 
 from foxylib.tools.native.class_tools import ClassToolkit
 from foxylib.tools.native.function_tools import FunctionToolkit
+
 
 class LoggerToolkit:
     @classmethod
@@ -52,16 +51,70 @@ class LoggerToolkit:
             logger.addHandler(handler)
 
     @classmethod
-    def func2logger(cls, func, handlers=None, level=None,):
-        name = cls.func2name(func)
-        logger = logging.getLogger(name)
-
-        cls.add_or_skip_handlers(logger, handlers)
+    def kwargs2logger(cls, logger, handlers=None, level=None,):
+        if handlers:
+            cls.add_or_skip_handlers(logger, handlers)
 
         if level is not None:
             logger.setLevel(level)
 
         return logger
+
+    @classmethod
+    def func2logger(cls, func,):
+        name = cls.func2name(func)
+        logger = logging.getLogger(name)
+        return logger
+
+    @classmethod
+    def func2init_logger(cls, func=None, func2logger=None,):
+        def wrapper(f):
+            _logger = None
+            _func2logger = func2logger if func2logger else cls.func2logger
+            @wraps(f)
+            def wrapped(*args, **kwargs):
+                nonlocal _logger
+                if _logger is None:
+                    _logger = _func2logger(f)
+
+                result = f(*args, **kwargs)
+                return result
+
+            return wrapped
+
+        return wrapper(func) if func else wrapper
+
+    @classmethod
+    def log_exec_duration(cls, func=None, msg=None, logger=None, ):
+        def wrapper(f):
+            _logger = logger
+            _msg = msg
+
+            @wraps(f)
+            def wrapped(*args, **kwargs):
+                nonlocal _logger
+                if _logger is None:
+                    _logger = cls.func2logger(f)
+
+                nonlocal _msg
+                if _msg is None:
+                    _msg = "[{0}] exec time".format(FunctionToolkit.func2name(f))
+
+                dt_start = datetime.now()
+                result = f(*args, **kwargs)
+                td_exec = datetime.now() - dt_start
+
+                _logger.info({
+                    'message': _msg,
+                    'exec_ms': round(td_exec.total_seconds() * 1000, 3),
+                })
+                return result
+
+            return wrapped
+
+        return wrapper(func) if func else wrapper
+
+
 
 
 
