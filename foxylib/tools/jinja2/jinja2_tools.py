d812f1592c5a22d57ec94a2952141d31e2efe78b
yerihyo <yerihyo@gmail.com>
2019-03-01 19:30:35 -0800

    (#1) adding test for jinja2

--- foxylib/tools/jinja2/jinja2_tools.py
+++ foxylib/tools/jinja2/jinja2_tools.py
@@ -2,7 +2,15 @@ from jinja2 import Environment, BaseLoader
 
 class Jinja2Toolkit:
     @classmethod
-    def str_tmplt2render(cls, str_tmplt, data):
+    def str_tmplt2str(cls, str_tmplt, data):
         rtemplate = Environment(loader=BaseLoader).from_string(str_tmplt)
         s_out = rtemplate.render(**data)
         return s_out
+
+    @classmethod
+    def file_tmplt2str(cls, filepath, data):
+        rtemplate = Environment(loader=BaseLoader).get_template(filepath)
+        s_out = rtemplate.render(**data)
+        return s_out
+
+str_tmplt2str = Jinja2Toolkit.str_tmplt2str
