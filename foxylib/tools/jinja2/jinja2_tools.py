40383e6342cf1e5897120635e780b0d1c810203e
Moonyoung Kang <yerihyo@gmail.com>
2019-04-05 15:29:14 -0700

    (#3) update

--- foxylib/tools/jinja2/jinja2_tools.py
+++ foxylib/tools/jinja2/jinja2_tools.py
@@ -1,22 +1,57 @@
 import os
+from functools import lru_cache
 
-from jinja2 import Environment, BaseLoader, FileSystemLoader
+from jinja2 import Environment, BaseLoader, FileSystemLoader, Template
+
+from foxylib.tools.file.file_tools import FileToolkit
 
 
 class Jinja2Toolkit:
     @classmethod
-    def tmplt_str2str(cls, str_tmplt, data):
-        rtemplate = Environment(loader=BaseLoader).from_string(str_tmplt)
-        s_out = rtemplate.render(**data)
-        return s_out
+    @lru_cache(maxsize=2)
+    def _js_escapes(cls):
+        h = {
+            '\\': '\\u005C',
+            '\'': '\\u0027',
+            '"': '\\u0022',
+            '>': '\\u003E',
+            '<': '\\u003C',
+            '&': '\\u0026',
+            '=': '\\u003D',
+            '-': '\\u002D',
+            ';': '\\u003B',
+            u'\u2028': '\\u2028',
+            u'\u2029': '\\u2029'
+        }
+        # Escape every ASCII character with a value less than 32.
+        h.update(('%c' % z, '\\u%04X' % z) for z in range(32))
+        return h
+
+    @classmethod
+    def escape_js(cls, value):
+        if value is None:
+            return None
+
+        if not isinstance(value, str):
+            return value
+
+        return value.replace('"', r'\"')
+
+    @classmethod
+    def data2js_escaped(cls, data):
+        return {k:cls.escape_js(v) for k,v in data.items()}
+
+    @classmethod
+    def tmplt_str2str(cls, str_tmplt, data, autoescape=False):
+        template = Template(str_tmplt, autoescape=autoescape)
+        data_escaped = cls.data2js_escaped(data)
+        return template.render(**data_escaped)
 
     @classmethod
     def tmplt_file2str(cls, filepath, data):
-        file_dir, base_name = os.path.split(filepath)
-        loader = FileSystemLoader(file_dir)
-        rtemplate = Environment(loader=loader).get_template(base_name)
-        s_out = rtemplate.render(**data)
-        return s_out
+        str_tmplt = FileToolkit.filepath2utf8(filepath)
+        return cls.tmplt_str2str(str_tmplt, data)
+
 
 tmplt_str2str = Jinja2Toolkit.tmplt_str2str
 tmplt_file2str = Jinja2Toolkit.tmplt_file2str
---
+++ foxylib/tools/thread/thread_tools.py
@@ -0,0 +1,42 @@
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from functools import wraps
+
+from foxylib.tools.log.logger_tools import LoggerToolkit, FoxylibLogger
+
+logger = logging.getLogger(__name__)
+
+class ThreadToolkit:
+    @classmethod
+    def func2threaded(cls, func=None, max_workers=None, ):
+        logger = FoxylibLogger.func2logger(cls.func2threaded)
+
+        def wrapper(f):
+            @wraps(f)
+            def wrapped(*args, **kwargs):
+                executor = ThreadPoolExecutor(max_workers=max_workers)  # non-blocking
+
+                def f_new(*args, **kwargs):
+                    rv = f(*args, **kwargs)
+
+                    logger.info({"message": "func2thread", "value": rv})
+                    LoggerToolkit.logger2flush_handlers(logger)
+
+                    return rv
+
+                future = executor.submit(f_new, *args, **kwargs)
+                # future.add_done_callback(lambda x: rs.setex(name, time, x.result()))
+                return future
+
+            return wrapped
+
+        return wrapper(func) if func else wrapper
+
+    @classmethod
+    def future2result_or_raise(cls, future):
+        exc_future = future.exception()
+        if exc_future:
+            logger.error({"messsage":"future2result_or_raise", "exception":exc_future})
+            raise exc_future
+
+        return future.result()
