6d8b92ba3feb94f8ac4beec3c07764b177d76913
Moonyoung Kang <yerihyo@gmail.com>
2019-07-16 10:06:04 -0700

    (#5) small change

--- scripts/lpass/login.bash
+++ scripts/lpass/login.bash
@@ -7,31 +7,26 @@ FILE_NAME=$(basename $FILE_PATH)
 FILE_DIR=$(dirname $FILE_PATH)
 LPASS_DIR=$FILE_DIR
 
-help_message() {
-	echo ""
-	echo "usage: $ARG0 <username> [password]"
-	echo ""
+errcho(){ >&2 echo $@; }
+usage(){ errcho "usage: $ARG0 <username> [password]"; }
+main(){
+    if [[ "$password" ]]; then
+        errcho "$password" | LPASS_DISABLE_PINENTRY=1 lpass login "$username"
+    else
+        lpass login "$username"
+    fi
 }
+#readonly _username="${LPASS_USERNAME:-}"
+#readonly _password="${LPASS_PASSWORD:-}"
 
-readonly _username="${LPASS_USERNAME:-}"
-readonly _password="${LPASS_PASSWORD:-}"
-
-readonly username="${1:-$_username}"
-readonly password="${2:-$_password}"
+readonly username="${1:-${LPASS_USERNAME:-}}"
+readonly password="${2:-${LPASS_PASSWORD:-}}"
 
 if [[ ! "$username" ]]; then help_message; exit; fi
 
-echo "[$FILE_NAME] START (username=$username)"
-
-# lpass logout -f
-if [[ "$password" ]]; then
-    echo "$password" \
-	| LPASS_DISABLE_PINENTRY=1 lpass login "$username"
-else
-    lpass login "$username"
-fi
-
-echo "[$FILE_NAME] END"
+errcho "[$FILE_NAME] START (username=$username)"
+main
+errcho "[$FILE_NAME] END"
 
 
 
