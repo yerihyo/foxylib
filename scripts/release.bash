1f0c4924d7fd8f430a9ddb73ba2a633095ad3130
yerihyo <yerihyo@gmail.com>
2019-12-12 10:31:46 -0800

    added release script

--- scripts/release.bash
+++ scripts/release.bash
@@ -1,12 +1,45 @@
 #!/bin/bash -eu
 
-POST https://api.github.com/repos/yerihyo/foxylib/releases
+FILE_PATH=$(readlink -f $0)
+FILE_NAME=$(basename $FILE_PATH)
+FILE_DIR=$(dirname $FILE_PATH)
+# FILE_DIR=`pwd`/../scripts/test
+SCRIPTS_DIR=$FILE_DIR
 
-{
-  "tag_name": "v0.3.74",
+errcho(){ >&2 echo $@; }
+func_count2reduce(){
+    local v="${1?missing}"; local cmd="${2?missing}"; local n=${3?missing};
+    for ((i=0;i<$n;i++)); do v=$($cmd $v) ; done; echo "$v"
+}
+
+REPO_DIR=$(func_count2reduce $FILE_DIR dirname 1)
+
+
+GITHUB_OAUTH_TOKEN="${GITHUB_OAUTH_TOKEN?missing token}"
+version="${1?missing version}"
+
+post(){
+    curl -X POST \
+	 https://api.github.com/repos/yerihyo/foxylib/releases \
+	 -H 'Accept: */*' \
+	 -H 'Accept-Encoding: gzip, deflate' \
+	 -H "Authorization: token $token" \
+	 -H 'Cache-Control: no-cache' \
+	 -H 'Connection: keep-alive' \
+	 -H 'Content-Length: 144' \
+	 -H 'Content-Type: application/json' \
+	 -H 'Host: api.github.com' \
+	 -H 'cache-control: no-cache' \
+	 -d '{
+  "tag_name": "'$tag_name'",
   "target_commitish": "develop",
-  "name": "v0.3.74",
+  "name": "'$tag_name'",
   "body": "Release",
   "draft": false,
   "prerelease": false
 }
+'
+}
+
+
+post
