465b33291ddce71803ca3914d5573798780b63d8
Moonyoung Kang <yerihyo@gmail.com>
2020-01-15 22:46:34 -0800

    change test to consider loop

--- foxylib/tools/async/tests/test_async_tool.py
+++ foxylib/tools/async/tests/test_async_tool.py
@@ -17,19 +17,23 @@ class TestAsyncTool(TestCase):
     def test_01(self):
         cls = self.__class__
 
-        # loop = asyncio.get_event_loop() # not working. closed when test closes
-        loop = asyncio.new_event_loop()
-        self.assertFalse(loop.is_closed())
+        def subtest(loop):
+            self.assertFalse(loop.is_closed())
 
-        tasks = [asyncio.ensure_future(cls.countdown_coroutine("A",2), loop=loop),
-                 asyncio.ensure_future(cls.countdown_coroutine("B", 3), loop=loop),
-                 ]
-        loop.run_until_complete(asyncio.wait(tasks))
-        loop.close()
+            tasks = [asyncio.ensure_future(cls.countdown_coroutine("A",2), loop=loop),
+                     asyncio.ensure_future(cls.countdown_coroutine("B", 3), loop=loop),
+                     ]
+            loop.run_until_complete(asyncio.wait(tasks))
 
-        print("hello world", file=sys.stderr)
+            print("hello world", file=sys.stderr)
 
+        loop_system = asyncio.get_event_loop()
+        subtest(loop_system)
+        # loop_system.close()  # do not close the loop if using asyncio.get_event_loop()
 
+        loop_new = asyncio.new_event_loop()  # not working. closed when test closes
+        subtest(loop_new)
+        loop_new.close()  # can close because loop is from asyncio.new_event_loop()
 
     @classmethod
     async def countdown_async(cls, number, n):
@@ -42,30 +46,43 @@ class TestAsyncTool(TestCase):
     def test_02(self):
         cls = self.__class__
 
-        # loop = asyncio.get_event_loop() # not working. closed when test closes
-        loop = asyncio.new_event_loop()
-        self.assertFalse(loop.is_closed())
+        def subtest(loop):
+            self.assertFalse(loop.is_closed())
 
 
-        futures = [cls.countdown_async("A", 2),
-                   cls.countdown_async("B", 3),
-                   ]
-        loop.run_until_complete(asyncio.wait(futures))
-        loop.close()  # ?? https://stackoverflow.com/questions/51271477/python3-x-runtimeerror-event-loop-is-closed
+            futures = [cls.countdown_async("A", 2),
+                       cls.countdown_async("B", 3),
+                       ]
+            loop.run_until_complete(asyncio.wait(futures))
+            print("hello world", file=sys.stderr)
 
-        print("hello world", file=sys.stderr)
+        loop_system = asyncio.get_event_loop()
+        subtest(loop_system)
+        # loop_system.close()  # do not close the loop if using asyncio.get_event_loop()
+
+        loop_new = asyncio.new_event_loop()  # not working. closed when test closes
+        subtest(loop_new)
+        loop_new.close()  # can close because loop is from asyncio.new_event_loop()
+
 
     def test_03(self):
         cls = self.__class__
 
-        # loop = asyncio.get_event_loop() # not working. closed when test closes
-        loop = asyncio.new_event_loop()
-        self.assertFalse(loop.is_closed())
+        def subtest(loop):
+            self.assertFalse(loop.is_closed())
 
-        tasks = [asyncio.ensure_future(cls.countdown_async("A",2), loop=loop),
-                 asyncio.ensure_future(cls.countdown_async("B", 3), loop=loop),
-                 ]
-        loop.run_until_complete(asyncio.gather(*tasks))
-        loop.close()
+            tasks = [asyncio.ensure_future(cls.countdown_async("A",2), loop=loop),
+                     asyncio.ensure_future(cls.countdown_async("B", 3), loop=loop),
+                     ]
+            loop.run_until_complete(asyncio.gather(*tasks))
 
-        print("hello world", file=sys.stderr)
+            print("hello world", file=sys.stderr)
+
+        loop_system = asyncio.get_event_loop()
+        subtest(loop_system)
+        # loop_system.close()  # do not close the loop if using asyncio.get_event_loop()
+
+        loop_new = asyncio.new_event_loop()  # not working. closed when test closes
+        subtest(loop_new)
+        loop_new.close()  # can close because loop is from asyncio.new_event_loop()
+
