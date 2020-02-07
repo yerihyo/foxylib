1af28da85cd10bc111bb0b57119c5710c4c08b09
Moonyoung Kang <yerihyo@gmail.com>
2020-02-06 23:57:22 -0800

    relocate

--- scripts/git/feature2develop.bash
+++ scripts/git/feature2develop.bash
@@ -5,10 +5,6 @@ set -u
 
 FILE_DIR=$(dirname `readlink -f ${0}`)
 # FILE_DIR=`pwd`/../scripts
-SCRIPTS_DIR=$FILE_DIR
-BASE_DIR=$(dirname $SCRIPTS_DIR)
-WEB_DIR=$BASE_DIR/web
-LOG_DIR=$WEB_DIR/log
 
 branch=`git branch | grep '\*' | awk '{print $2}'`
 
