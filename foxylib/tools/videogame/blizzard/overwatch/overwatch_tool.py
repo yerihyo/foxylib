1af1772a297ddaa889a73527f9e150b8a0e05162
yerihyo <yerihyo@gmail.com>
2020-02-12 21:01:31 -0800

    added owapi

--- foxylib/tools/videogame/blizzard/overwatch/overwatch_tool.py
+++ foxylib/tools/videogame/blizzard/overwatch/overwatch_tool.py
@@ -1,24 +1,28 @@
+from foxylib.tools.http.http_tool import HttpTool
+from foxylib.tools.string.string_tool import format_str
+
 from foxylib.tools.collections.collections_tool import lzip_strict
 
+class BattletagTool:
+    @classmethod
+    def battletag2hyphened(cls, battletag):
+        return battletag.replace("#","-")
 
 # https://github.com/Fuyukai/OWAPI/blob/master/api.md
+class OwapiTool:
+    @classmethod
+    def battletag2j_blob(cls, battletag):
+        url = format_str("https://owapi.net/api/v3/u/{}/blob",
+                         BattletagTool.battletag2hyphened(battletag))
+
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        kwargs = {"headers":{'User-Agent': user_agent}}
+        httpr = HttpTool.url2httpr(url, kwargs=kwargs)
+        j_blob = httpr.json()
+        return j_blob
+
+
 class OverwatchTool:
     @classmethod
-    def tier_list(cls,):
-        return ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Grandmaster", "Top500"]
-
-    @classmethod
-    def lang2tier_value_name_list(cls, lang):
-        v_list = cls.tier_list()
-
-
-        if not lang or lang == "en":
-            return [(x,x) for x in v_list]
-
-        if lang == "ko":
-            name_list = ["브론즈","실버","골드","플래티넘","다이아몬드","마스터","그랜드마스터","랭커"]
-            return lzip_strict(v_list, name_list)
-
-        raise NotImplementedError({"lang":lang})
-
-
+    def battletag_test(cls):
+        return "yeri#11211"
--- foxylib/tools/videogame/blizzard/overwatch/tests/test_overwatch_tool.py
+++ foxylib/tools/videogame/blizzard/overwatch/tests/test_overwatch_tool.py
@@ -1,31 +1,21 @@
+import logging
 from asyncio import gather
 from pprint import pprint
 from unittest import TestCase
 
 from foxylib.tools.asyncio.asyncio_tool import AsyncioTool
+from foxylib.tools.log.foxylib_logger import FoxylibLogger
+from foxylib.tools.videogame.blizzard.overwatch.overwatch_tool import OwapiTool, OverwatchTool
 
 
-class TestOverwatchTool(TestCase):
-    pass
-    
-    # def test_01(self):
-    #     from overwatch import Overwatch
-    #     overwatch = Overwatch(battletag="Okush#11324", mode="competitive")
-    #     print(overwatch.playtime)
+class TestOwapiTool(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        FoxylibLogger.attach_stderr2loggers(logging.DEBUG)
 
-    # def test_02(self):
-    #     import asyncio, aiohttp
-    #     from overwatch_api.core import AsyncOWAPI
-    #     from overwatch_api.constants import PC, XBOX, PLAYSTATION
-    #
-    #     client = AsyncOWAPI()
-    #
-    #     profile = AsyncioTool.awaitable2result(client.get_profile("yeri#11211", platform=PC))
-    #
-    #     tasks = [client.get_profile("Danielfrogs#2552", platform=PC),
-    #              client.get_stats("Danielfrogs#2552", platform=XBOX),
-    #              client.get_achievements("Danielfrogs#2552", platform=PLAYSTATION),
-    #              client.get_hero_stats("Danielfrogs#2552"),
-    #              ]
-    #     result_list = AsyncioTool.coroutine_list2result_list(tasks)
-    #     pprint(result_list)
+    def test_01(self):
+        logger = FoxylibLogger.func_level2logger(self.test_01, logging.DEBUG)
+        j_blob = OwapiTool.battletag2j_blob("yeri#11211")
+
+        logger.debug({"j_blob":j_blob})
+        self.assertFalse({"kr","eu","us"} - set(j_blob.keys()))
