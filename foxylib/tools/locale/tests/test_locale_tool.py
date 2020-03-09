a42c190fa596407f79ee478554c7e159f1e9d91b
yerihyo <yerihyo@gmail.com>
2020-03-09 09:45:03 -0700

    changing travis yaml

--- foxylib/tools/locale/tests/test_locale_tool.py
+++ foxylib/tools/locale/tests/test_locale_tool.py
@@ -46,10 +46,10 @@ class MyLocale:
 
 class TestLocaleTool(TestCase):
     def test_01(self):
-        with LocaleTool.override("en_US", category=locale.LC_CTYPE):
+        with LocaleTool.override("en_US", category=locale.LC_ALL):
             self.assertEqual(MyLocale.gettext("hello"), "hello")
 
-        with LocaleTool.override("ko_KR", category=locale.LC_CTYPE):
+        with LocaleTool.override("ko_KR", category=locale.LC_ALL):
             self.assertEqual(MyLocale.gettext("goodbye"), "안녕")
 
 
