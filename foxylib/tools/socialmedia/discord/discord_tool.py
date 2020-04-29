c940331b833436cc7a7e05527b910cd75e4b4f4c
yerihyo <yerihyo@gmail.com>
2020-04-28 23:32:02 -0700

    deploy

--- foxylib/tools/socialmedia/discord/discord_tool.py
+++ foxylib/tools/socialmedia/discord/discord_tool.py
@@ -1,3 +1,8 @@
+import logging
+import sys
+from functools import lru_cache
+
+from foxylib.tools.log.logger_tool import LoggerTool, FoxylibLogFormatter
 from foxylib.tools.url.url_tool import URLTool
 
 
@@ -16,3 +21,37 @@ class DiscordTool:
     @classmethod
     def user_message2is_author(cls, user, message):
         return message.author == user
+
+
+class DiscordLogger:
+    ROOTNAME = "discord"
+
+    @classmethod
+    def rootname_list(cls):
+        return [cls.ROOTNAME]
+
+    @classmethod
+    def attach_handler2loggers(cls, handler):
+        for rootname in cls.rootname_list():
+            logger = logging.getLogger(rootname)
+            LoggerTool.add_or_skip_handlers(logger, [handler])
+
+    @classmethod
+    @lru_cache(maxsize=2)
+    def attach_stderr2loggers(cls, level):
+        handler = LoggerTool.handler_formatter2formatted(logging.StreamHandler(sys.stderr),
+                                                         FoxylibLogFormatter.formatter(),
+                                                         )
+        handler.setLevel(level)
+        cls.attach_handler2loggers(handler)
+
+
+    # @classmethod
+    # def func2name(cls, func):
+    #     return LoggerTool.rootname_func2name(cls.ROOTNAME, func)
+    #
+    # @classmethod
+    # def func_level2logger(cls, func, level):
+    #     logger = logging.getLogger(cls.func2name(func))
+    #     logger.setLevel(level)
+    #     return logger
