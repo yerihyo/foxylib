4a524aca7d5d93002d89d145a0b3798c701bb2b9
yerihyo <yerihyo@gmail.com>
2020-05-26 22:48:31 -0700

    small change

--- foxylib/tools/googleapi/sheets/googlesheets_tool.py
+++ foxylib/tools/googleapi/sheets/googlesheets_tool.py
@@ -1,16 +1,18 @@
 import logging
+from pprint import pprint
 
+from future.utils import lmap
 from googleapiclient.discovery import build
 
-from foxylib.tools.collections.collections_tool import merge_dicts, vwrite_no_duplicate_key
+from foxylib.tools.collections.collections_tool import merge_dicts, vwrite_no_duplicate_key, DictTool, zip_strict
 from foxylib.tools.log.foxylib_logger import FoxylibLogger
 from httplib2 import Http
 
 
 class GooglesheetsTool:
     @classmethod
-    def cred_id_name2data_ll(cls, credentials, spreadsheet_id, range):
-        logger = FoxylibLogger.func_level2logger(cls.cred_id_name2data_ll, logging.DEBUG)
+    def sheet_range2data_ll(cls, credentials, spreadsheet_id, range):
+        logger = FoxylibLogger.func_level2logger(cls.sheet_range2data_ll, logging.DEBUG)
         logger.debug({"spreadsheet_id": spreadsheet_id, "range": range})
 
         # service = build('sheets', 'v4', http=credentials.authorize(Http()))
@@ -25,6 +27,31 @@ class GooglesheetsTool:
         logger.debug({"len(values)":len(values)})
 
         return values
+
+    @classmethod
+    def sheet_ranges2data_lll(cls, credentials, spreadsheet_id, ranges=None):
+        logger = FoxylibLogger.func_level2logger(cls.sheet_ranges2data_lll, logging.DEBUG)
+        logger.debug({"spreadsheet_id": spreadsheet_id, "ranges": ranges})
+
+        # service = build('sheets', 'v4', http=credentials.authorize(Http()))
+        service = build('sheets', 'v4', credentials=credentials)
+
+        h = DictTool.filter(lambda k, v: v,
+                            {"spreadsheetId": spreadsheet_id,
+                             "ranges": ranges,
+                             })
+        result = service.spreadsheets().values().batchGet(**h).execute()
+        # pprint({"result":result})
+        values = lmap(lambda h:h.get("values") or [], result.get('valueRanges', []))
+
+        logger.debug({"len(values)": len(values)})
+
+        return values
+
+    @classmethod
+    def sheet_ranges2dict_range2data_ll(cls, credentials, spreadsheet_id, ranges):
+        data_lll = cls.sheet_ranges2data_lll(credentials, spreadsheet_id, ranges)
+        return dict(zip_strict(ranges, data_lll))
 
 
     @classmethod
