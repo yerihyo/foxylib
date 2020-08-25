cdb04f92d5f2e60bb59206258e5a70f66cac3560
yerihyo <yerihyo@gmail.com>
2020-08-25 09:58:23 -0700

    (PTAH-2) add test

--- foxylib/tools/cache/tests/test_asymnmetric_cache.py
+++ foxylib/tools/cache/tests/test_asymnmetric_cache.py
@@ -1,4 +1,5 @@
 import logging
+from functools import lru_cache
 from unittest import TestCase
 
 from cachetools import LRUCache
@@ -46,3 +47,46 @@ class TestAsymmetricCache(TestCase):
         self.assertEqual(c[(3,)], 4)
         self.assertEqual(f(3), 3)
         self.assertEqual(c[(3,)], 3)
+
+    def test_02(self):
+
+        def reader(cache, x, *_, **__):
+            if x == 3:
+                raise KeyError(x)
+
+            return cache[(x,)]
+
+        def writer(cache, value, x, *_, **__):
+            if x == 2:
+                return
+
+            k = (x,)
+            cache[k] = value
+
+        class A:
+            @lru_cache(maxsize=2)  # memleak possible, but using it only for testing
+            def cache(self):
+                return LRUCache(10)
+
+            @AsymmetricCache.Decorator.cachedmethod(self2cache=lambda x:x.cache(), reader=reader, writer=writer)
+            def f(self, x):
+                return x
+
+        a1 = A()
+        a2 = A()
+
+        a1.cache()[(1,)] = 2
+        self.assertEqual(a1.f(1), 2)
+        self.assertEqual(a2.f(1), 1)
+        self.assertEqual(a1.cache()[(1,)], 2)
+        del a1.cache()[(1,)]
+        self.assertEqual(a1.f(1), 1)
+
+        self.assertEqual(a1.f(2), 2)
+        self.assertNotIn(2, a1.cache())
+
+        self.assertEqual(a1.f(3), 3)
+        a1.cache()[(3,)] = 4
+        self.assertEqual(a1.cache()[(3,)], 4)
+        self.assertEqual(a1.f(3), 3)
+        self.assertEqual(a1.cache()[(3,)], 3)
