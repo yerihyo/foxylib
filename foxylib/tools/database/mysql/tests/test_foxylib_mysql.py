341c568ad2a3e32a5bcd09c29a6ead61b424d73d
yerihyo <yerihyo@gmail.com>
2020-10-10 08:00:23 -0700

    (T16) update

--- foxylib/tools/database/mysql/tests/test_foxylib_mysql.py
+++ foxylib/tools/database/mysql/tests/test_foxylib_mysql.py
@@ -1,6 +1,8 @@
 import logging
 from unittest import TestCase
 
+import pytest
+
 from foxylib.tools.database.mysql.foxylib_mysql import SampleTable
 from foxylib.tools.log.foxylib_logger import FoxylibLogger
 
@@ -10,6 +12,7 @@ class TestFoxylibMysql(TestCase):
     def setUpClass(cls):
         FoxylibLogger.attach_stderr2loggers(logging.DEBUG)
 
+    @pytest.mark.skip(reason="not connected to MySQL")
     def test_01(self):
         with SampleTable.values2cursor(['a','b','c','d','e']) as c:
             self.assertTrue(list(c))
