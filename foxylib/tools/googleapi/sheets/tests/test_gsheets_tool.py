a9b1f7b3b9e9dfe86680030917ef0ca3566f5f70
Moonyoung Kang <moonyoungkang@Moonyoungs-MacBook-Pro.local>
2023-04-05 12:51:36 -0700

    (t264) gsheets update

--- foxylib/tools/googleapi/sheets/tests/test_gsheets_tool.py
+++ foxylib/tools/googleapi/sheets/tests/test_gsheets_tool.py
@@ -48,13 +48,74 @@ class TestNative(TestCase):
         pprint(response)
 
 
+    def test_3(self):
+        logger = FoxylibLogger.func_level2logger(self.test_3, logging.DEBUG)
+
+        cred = FoxylibGoogleapi.ServiceAccount.credentials()
+        service = build('sheets', 'v4', credentials=cred, cache_discovery=False)
+
+        sheetId = GsheetsTool.sheetname2sheetid(service, '19-7_V7N89Tou9v_SeMv4g1AvNZ7rnEdYNCvI9bYFDgo', 'testlink')
+
+        request = service.spreadsheets().batchUpdate(**{
+            "spreadsheetId": '19-7_V7N89Tou9v_SeMv4g1AvNZ7rnEdYNCvI9bYFDgo',
+            "body": {
+                "requests": [{
+                    'updateCells': {
+                        'range': {
+                            'sheetId': sheetId,
+                            # 'startRowIndex': 0,
+                            # 'startColumnIndex': 0,
+                        },
+                        'fields':'*',
+                        'rows': [{
+                            'values': [{
+                                'userEnteredValue': {'stringValue': """asdf qwer
+asdf"""},
+                                'textFormatRuns': [
+                                    {'startIndex': 0, 'format': {'link': {'uri': 'https://www.google.com'}}},
+                                    {'startIndex': 4, },
+                                    {'startIndex': 11, 'format': {'link': {'uri': 'https://www.naver.com'}}},
+                                ]
+                            }]
+                        }]
+                    }
+                }]
+            }
+        })
+
+        # request = service.spreadsheets().values().update(**{
+        #     "spreadsheetId": '19-7_V7N89Tou9v_SeMv4g1AvNZ7rnEdYNCvI9bYFDgo',
+        #     "range": 'testlink',
+        #     "valueInputOption": "USER_ENTERED",
+        #     # https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+        #     "body": {
+        #         'range': 'testlink',
+        #         # "majorDimension": "COLUMNS",
+        #         'values': [
+        #             [
+        #                 {
+        #                     'userEnteredValue': {'stringValue': 'asdf qwer asdf'},
+        #                     'textFormatRuns': [
+        #                         {'startIndex': 0, 'format': {'link': {'uri': 'http:www.google.com'}}},
+        #                         {'startIndex': 4, },
+        #                     ]
+        #                 }
+        #             ],
+        #         ]
+        #     },
+        # })
+        response = request.execute()
+
+        pprint(response)
+
+
 class TestGooglesheetsTool(TestCase):
     @classmethod
     def setUpClass(cls):
         FoxylibLogger.attach_stderr2loggers(logging.DEBUG)
 
-    def test_01(self):
-        logger = FoxylibLogger.func_level2logger(self.test_01, logging.DEBUG)
+    def test_1(self):
+        logger = FoxylibLogger.func_level2logger(self.test_1, logging.DEBUG)
 
         cred = FoxylibGoogleapi.ServiceAccount.credentials()
         hyp = GsheetsTool.sheet_range2data_ll(cred, "19-7_V7N89Tou9v_SeMv4g1AvNZ7rnEdYNCvI9bYFDgo", "Sheet1")
@@ -63,8 +124,8 @@ class TestGooglesheetsTool(TestCase):
         # pprint(hyp)
         self.assertEquals(hyp, ref)
 
-    def test_02(self):
-        logger = FoxylibLogger.func_level2logger(self.test_02, logging.DEBUG)
+    def test_2(self):
+        logger = FoxylibLogger.func_level2logger(self.test_2, logging.DEBUG)
 
         cred = FoxylibGoogleapi.ServiceAccount.credentials()
         hyp = GsheetsTool.sheet_ranges2data_lll(cred, "19-7_V7N89Tou9v_SeMv4g1AvNZ7rnEdYNCvI9bYFDgo", ["Sheet1", "Sheet2","a"])
@@ -77,8 +138,8 @@ class TestGooglesheetsTool(TestCase):
         self.assertEquals(hyp, ref)
 
 
-    def test_03(self):
-        logger = FoxylibLogger.func_level2logger(self.test_02, logging.DEBUG)
+    def test_3(self):
+        logger = FoxylibLogger.func_level2logger(self.test_3, logging.DEBUG)
 
         cred = FoxylibGoogleapi.ServiceAccount.credentials()
         hyp = GsheetsTool.sheet_ranges2dict_range2data_ll(cred, "19-7_V7N89Tou9v_SeMv4g1AvNZ7rnEdYNCvI9bYFDgo", ["Sheet1", "Sheet2","a"])
@@ -98,7 +159,7 @@ class TestGooglesheetsTool(TestCase):
         self.assertEqual(GsheetsTool.colindex2name(26 + 26 * 26 + 26 * 26 * 26), 'AAAA')
 
     def test_5(self):
-        logger = FoxylibLogger.func_level2logger(self.test_01, logging.DEBUG)
+        logger = FoxylibLogger.func_level2logger(self.test_5, logging.DEBUG)
 
         cred = FoxylibGoogleapi.ServiceAccount.credentials()
         service = GsheetsTool.credentials2service(cred)
